bd94d2fa4b458406196110d5f2f4b518c79f7d53
Kirsty <kkjemma01@gmail.com>
2020-06-25 17:02:42 +0100

    Admins can now add comments

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 @login_required
 def post_new(request):
     if request.method == "POST":
